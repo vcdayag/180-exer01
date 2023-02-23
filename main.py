ed28fa6e3752d422e6d74945201a4f6222041921
Van Paul Dayag <vcdayag@up.edu.ph>
2023-02-23 15:18:46 +0800

    more stuff

--- main.py
+++ main.py
@@ -8,11 +8,17 @@ def generateMatrix(n:int) -> list[list[float]]:
     for x in range(n//10+1):
         for y in range(n//10+1):
             matrix[x*10][y*10] = random.randint(1,1000)
+    
+    matrix[0][0] = 200
+    matrix[0][10] = 500
+    matrix[10][0] = 50
+    matrix[10][10] = 10
+
 
     return matrix
 
 # Area Weighted Interpolation
-def awi(M,row,col) -> float:
+def awi(M,n,row,col) -> float:
     # index of the top left given point
     sectionrow = row//10
     sectioncol = col//10
@@ -20,10 +26,15 @@ def awi(M,row,col) -> float:
     incrementrow = 10
     incrementcol = 10
 
-    if sectionrow*10 == row and row != 0:
+    if sectionrow*10 == row and row != 0 and row != (n-1)*10:
         incrementrow = 0
-    if sectioncol*10 == col and col != 0:
+        sectionrow -= 1
+    if sectioncol*10 == col and col != 0 and row != (n-1)*10:
         incrementcol = 0
+        sectioncol -= 1
+    
+    sectioncol *= 10
+    sectionrow *= 10
 
 
     # top left
@@ -34,11 +45,12 @@ def awi(M,row,col) -> float:
     b = abs(sectionrow+incrementrow-row)*abs(sectioncol-col)
     # bottom right
     a = abs(sectionrow+incrementrow-row)*abs(sectioncol+incrementcol-col)
-
+    
     A = M[sectionrow][sectioncol]
     B = M[sectionrow][sectioncol+incrementcol]
     C = M[sectionrow+incrementrow][sectioncol]
     D = M[sectionrow+incrementrow][sectioncol+incrementcol]
+    print(a,b,c,d)
 
     return (a*A + b*B + c*C + d*D) / (a+b+c+d)
 
@@ -46,14 +58,16 @@ def terrain_inter(M:list[list[float]],n:int):
     for row in range(n):
         for col in range(n):
             if row%10 == 0 and col%10 == 0:
+                print("skip",row,col)
                 continue
-            M[row][col] = awi(M, row, col)
+            print(row,col)
+            M[row][col] = awi(M, n, row, col)
 
 if __name__ == "__main__":
     n = int(input("value of n: ")) + 1
     M = generateMatrix(n)
-    for x in M:
-        print(x)
+    # for x in M:
+        # print(x)
 
     time_before = time.time()
     terrain_inter(M,n)
