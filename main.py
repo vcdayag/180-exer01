147b8f7b46f3b55dcdc0266e45302108b0215577
Van Paul Dayag <vcdayag@up.edu.ph>
2023-02-23 14:46:11 +0800

    feat: impelement awi and terrain_inter

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-def generateMatrix(n:int) -> list[list[int]]:
+def generateMatrix(n:int) -> list[list[float]]:
 
     matrix = [ [0 for y in range(n)] for x in range(n)]
 
@@ -11,20 +11,59 @@ def generateMatrix(n:int) -> list[list[int]]:
 
     return matrix
 
-def fcc(x1,y1,h1,x2,y2,h2,interx,intery) -> float:
-    pass
+# Federal Communications Commission (FCC) Interpolation
+def fcc(x1,y1,h1,x2,y2,h2,x,y) -> float:
+    return y1 + (x-x1)/(x2-x1) * (y2-y1)
 
-def terrain_inter(M:list[list[int]]):
-    pass
+# Area Weighted Interpolation
+def awi(M,row,col) -> float:
+    # index of the top left given point
+    sectionrow = row//10
+    sectioncol = col//10
+
+    incrementrow = 10
+    incrementcol = 10
+
+    if sectionrow*10 == row and row != 0:
+        incrementrow = 0
+    if sectioncol*10 == col and col != 0:
+        incrementcol = 0
+
+
+    # top left
+    d = abs(sectionrow-row)*abs(sectioncol-col)
+    # top right
+    c = abs(sectionrow-row)*abs(sectioncol+incrementcol-col)
+    # bottom left
+    b = abs(sectionrow+incrementrow-row)*abs(sectioncol-col)
+    # bottom right
+    a = abs(sectionrow+incrementrow-row)*abs(sectioncol+incrementcol-col)
+
+    A = M[sectionrow][sectioncol]
+    B = M[sectionrow][sectioncol+incrementcol]
+    C = M[sectionrow+incrementrow][sectioncol]
+    D = M[sectionrow+incrementrow][sectioncol+incrementcol]
+
+    return (a*A + b*B + c*C + d*D) / (a+b+c+d)
+
+def terrain_inter(M:list[list[float]],n:int):
+    for row in range(n):
+        for col in range(n):
+            if row%10 == 0 and col%10 == 0:
+                continue
+            M[row][col] = awi(M, row, col)
 
 if __name__ == "__main__":
     n = int(input("value of n: ")) + 1
     M = generateMatrix(n)
-    # print(M)
+    print(M)
 
     time_before = time.time()
-    terrain_inter(M)
+    terrain_inter(M,n)
     time_after = time.time()
+
+    for x in M:
+        print(x)
 
     time_elapsed = time_after - time_before
 
