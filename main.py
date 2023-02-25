83fc5645b27355477cdef05addda436503da5c98
Van Paul Dayag <vcdayag@up.edu.ph>
2023-02-25 13:45:36 +0800

    refactor: remove debug statements and tests

--- main.py
+++ main.py
@@ -12,11 +12,6 @@ def generateMatrix(n: int) -> list[list[float]]:
         for y in range(n // 10 + 1):
             matrix[x * 10][y * 10] = random.randint(1, 1000)
 
-    matrix[0][0] = 200
-    matrix[0][10] = 500
-    matrix[10][0] = 50
-    matrix[10][10] = 10
-
     return matrix
 
 
@@ -24,12 +19,10 @@ def generateMatrix(n: int) -> list[list[float]]:
 def awi(M, row, col) -> float:
     incrementrow = 10
     incrementcol = 10
-    
+
     # get the coordinates of the nearest lower resolution point
     LRPOINTrow = row // 10 * 10
     LRPOINTcol = col // 10 * 10
-
-    # print(row,col)
 
     # check if the resolution point is the top left
     # if not update the LRPOINTrow or LRPOINTcol accordingly
@@ -69,22 +62,7 @@ def terrain_inter(M: list[list[float]], n: int):
 
 
 if __name__ == "__main__":
-    truevalue = [
-        [200, 230.0, 260.0, 290.0, 320.0, 350.0, 380.0, 410.0, 440.0, 470.0, 500],
-        [185.0, 211.6, 238.2, 264.8, 291.4, 318.0, 344.6, 371.2, 397.8, 424.4, 451.0],
-        [170.0, 193.2, 216.4, 239.6, 262.8, 286.0, 309.2, 332.4, 355.6, 378.8, 402.0],
-        [155.0, 174.8, 194.6, 214.4, 234.2, 254.0, 273.8, 293.6, 313.4, 333.2, 353.0],
-        [140.0, 156.4, 172.8, 189.2, 205.6, 222.0, 238.4, 254.8, 271.2, 287.6, 304.0],
-        [125.0, 138.0, 151.0, 164.0, 177.0, 190.0, 203.0, 216.0, 229.0, 242.0, 255.0],
-        [110.0, 119.6, 129.2, 138.8, 148.4, 158.0, 167.6, 177.2, 186.8, 196.4, 206.0],
-        [95.0, 101.2, 107.4, 113.6, 119.8, 126.0, 132.2, 138.4, 144.6, 150.8, 157.0],
-        [80.0, 82.8, 85.6, 88.4, 91.2, 94.0, 96.8, 99.6, 102.4, 105.2, 108.0],
-        [65.0, 64.4, 63.8, 63.2, 62.6, 62.0, 61.4, 60.8, 60.2, 59.6, 59.0],
-        [50, 46.0, 42.0, 38.0, 34.0, 30.0, 26.0, 22.0, 18.0, 14.0, 10],
-    ]
-    
-    # n = int(input("value of n: ")) + 1
-    n = 11
+    n = int(input("value of n: ")) + 1
     M = generateMatrix(n)
 
     time_before = time.time()
@@ -94,5 +72,3 @@ if __name__ == "__main__":
     time_elapsed = time_after - time_before
 
     print(f"Time elapsed: {time_elapsed} seconds")
-
-    print(truevalue == output)
