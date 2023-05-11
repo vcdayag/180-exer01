83b70efb6ad194a5e040308aa2874418cb8a3559
Van Paul Dayag <vcdayag@up.edu.ph>
2023-05-11 13:58:27 +0800

    feat: update running script

--- exer04.sh
+++ exer04.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gnome-terminal -- bash -c "python3 ./server.py; exec bash"
+gnome-terminal -- bash -c "python3 ./server.py $1 5050 1; exec bash"
 gnome-terminal -- bash -c "python3 ./client.py; exec bash"
