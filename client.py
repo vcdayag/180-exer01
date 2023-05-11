883dc159bfdb8092f65f47fbfdf3fc623b192765
Van Paul Dayag <vcdayag@up.edu.ph>
2023-05-11 13:58:01 +0800

    feat: update server and client

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import socket
+from array import array
+import interpolation
 
 host = '127.0.0.1'
 port = 5050
@@ -6,5 +8,9 @@ port = 5050
 s = socket.socket()
 s.connect((host,port))
 s.sendall(b'Hello, world')
-data = s.recv(1024)
+data = s.recv(64000)
+mat = eval(data)
+n = len(mat[0])
+output = interpolation.terrain_inter(mat,n,(0,n))
+print(output)
 
--- server.py
+++ server.py
@@ -1,7 +1,14 @@
 import socket
+import interpolation
+import sys
 
 host = '127.0.0.1'
-port = 5050
+# port = 5050
+
+n = int(sys.argv[1]) + 1
+port = int(sys.argv[2])
+# number of threads
+status = int(sys.argv[3])
 
 s = socket.socket()
 s.bind((host,port))
@@ -12,8 +19,12 @@ with conn:
     print("Connected by", addr)
     
     while True:
-        data = conn.recv(1024)
+        data = conn.recv(64000)
         if not data:
             break
+        print(data.decode())
         print("na send all")
-        conn.sendall(data)
+        mat = interpolation.generateMatrix(n)
+        
+        conn.sendall(str(mat).encode())
+        print(mat)
